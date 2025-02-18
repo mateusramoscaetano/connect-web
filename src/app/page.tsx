0ad9c9afe6890cc7bf350370a57d48e84f771b11
Mateus Caetano <mateus-ramoshd@hotmail.com>
2025-02-18 12:57:56 -0300

    add components to test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
+import { Button } from '@/components/button'
+import { IconButton } from '@/components/icon-button'
+import { InputField, InputIcon, InputRoot } from '@/components/input'
+import { ArrowRight, Copy, Mail } from 'lucide-react'
+
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+      <div>
+        <Button>
+          Enviar
+          <ArrowRight />
+        </Button>
+        <IconButton>
+          <Copy />
+        </IconButton>
+        <InputRoot>
+          <InputIcon>
+            <Mail />
+          </InputIcon>
+          <InputField placeholder="E-mail" />
+        </InputRoot>
+      </div>
     </main>
-  );
+  )
 }
