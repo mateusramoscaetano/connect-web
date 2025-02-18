bc5842e1e50c3d169234d9b107b9af31b3fef6f0
Mateus Caetano <mateus-ramoshd@hotmail.com>
2025-02-18 13:26:56 -0300

    Add home page

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -8,7 +8,6 @@ export function Button({ ...props }: Props) {
   return (
     <button
       {...props}
-      type="button"
       className="px-5 h-12 bg-gray-500 text-blue font-semibold rounded-xl w-full cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-300 relative flex justify-between items-center gap-2"
     />
   )
